57a0b02f2f6990e82bb7b495adf5f38d802279c2
Suraj Kumar Jana <surajjana2@gmail.com>
2016-03-20 22:29:31 +0530

    new commit

--- parse2.py
+++ parse2.py
@@ -7,7 +7,7 @@ workbook = xlwt.Workbook()
 
 for k in range(1,69):
 	url_symptoms = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/symptoms"
-	data_symptoms = {"request":{"user":{"age":8,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
+	data_symptoms = {"request":{"user":{"age":30,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
 	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 	r = requests.post(url_symptoms, data=json.dumps(data_symptoms), headers=headers)
 	r.encoding = None
@@ -37,10 +37,10 @@ for k in range(1,69):
 		data_id = l_s_id[i]
 		symptom_name = l_s_nm[i]
 		sheet.write(0,i,symptom_name,style)
-		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_7_12.xls')
+		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_25_34.xls')
 		print "Symptom ",i," : ", symptom_name
 		url_conditions = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/conditions"
-		data_conditions = {"request":{"user":{"age":8,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
+		data_conditions = {"request":{"user":{"age":30,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 		r = requests.post(url_conditions, data=json.dumps(data_conditions), headers=headers)
 		r.encoding = None
@@ -52,13 +52,13 @@ for k in range(1,69):
 			data2 = data["data"]["conditions"][j]["name"]
 			print j,". ",data2
 			sheet.write(j+1,i,data2)
-			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_7_12.xls')
+			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_25_34.xls')
 
 workbook = xlwt.Workbook()
 
 for k in range(1,69):
 	url_symptoms = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/symptoms"
-	data_symptoms = {"request":{"user":{"age":14,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
+	data_symptoms = {"request":{"user":{"age":40,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
 	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 	r = requests.post(url_symptoms, data=json.dumps(data_symptoms), headers=headers)
 	r.encoding = None
@@ -88,10 +88,10 @@ for k in range(1,69):
 		data_id = l_s_id[i]
 		symptom_name = l_s_nm[i]
 		sheet.write(0,i,symptom_name,style)
-		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_13_17.xls')
+		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_35_44.xls')
 		print "Symptom ",i," : ", symptom_name
 		url_conditions = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/conditions"
-		data_conditions = {"request":{"user":{"age":14,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
+		data_conditions = {"request":{"user":{"age":40,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 		r = requests.post(url_conditions, data=json.dumps(data_conditions), headers=headers)
 		r.encoding = None
@@ -103,13 +103,13 @@ for k in range(1,69):
 			data2 = data["data"]["conditions"][j]["name"]
 			print j,". ",data2
 			sheet.write(j+1,i,data2)
-			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_13_17.xls')
+			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_35_44.xls')
 
 workbook = xlwt.Workbook()
 
 for k in range(1,69):
 	url_symptoms = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/symptoms"
-	data_symptoms = {"request":{"user":{"age":20,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
+	data_symptoms = {"request":{"user":{"age":50,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
 	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 	r = requests.post(url_symptoms, data=json.dumps(data_symptoms), headers=headers)
 	r.encoding = None
@@ -139,10 +139,10 @@ for k in range(1,69):
 		data_id = l_s_id[i]
 		symptom_name = l_s_nm[i]
 		sheet.write(0,i,symptom_name,style)
-		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_18_24.xls')
+		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_45_54.xls')
 		print "Symptom ",i," : ", symptom_name
 		url_conditions = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/conditions"
-		data_conditions = {"request":{"user":{"age":20,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
+		data_conditions = {"request":{"user":{"age":50,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 		r = requests.post(url_conditions, data=json.dumps(data_conditions), headers=headers)
 		r.encoding = None
@@ -154,4 +154,106 @@ for k in range(1,69):
 			data2 = data["data"]["conditions"][j]["name"]
 			print j,". ",data2
 			sheet.write(j+1,i,data2)
-			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_18_24.xls')
+			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_45_54.xls')
+
+workbook = xlwt.Workbook()
+
+for k in range(1,69):
+	url_symptoms = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/symptoms"
+	data_symptoms = {"request":{"user":{"age":60,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
+	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	r = requests.post(url_symptoms, data=json.dumps(data_symptoms), headers=headers)
+	r.encoding = None
+	data = json.loads(r.text.encode(encoding='ISO-8859-1',errors='ignore'))
+
+	c = len(data["data"]["symptoms"])
+	i = 0
+	l_s_id = []
+	l_s_nm = []
+	for i in range(i,c):
+		data1 = data["data"]["symptoms"][i]["id"]
+		name1 = data["data"]["symptoms"][i]["nm"]
+		l_s_id.append(data1)
+		l_s_nm.append(name1)
+		#time.sleep(2)
+	#print l_s_id
+	#print l_s_nm
+
+	sheet = workbook.add_sheet("BodyPart_"+str(k))
+	style = xlwt.easyxf('font: bold 1')
+
+	print "Body Part : ",k,"\n\n"
+
+	i = 0
+
+	for i in range(i,c):
+		data_id = l_s_id[i]
+		symptom_name = l_s_nm[i]
+		sheet.write(0,i,symptom_name,style)
+		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_55_64.xls')
+		print "Symptom ",i," : ", symptom_name
+		url_conditions = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/conditions"
+		data_conditions = {"request":{"user":{"age":60,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		r = requests.post(url_conditions, data=json.dumps(data_conditions), headers=headers)
+		r.encoding = None
+		data = json.loads(r.text.encode(encoding='ISO-8859-1',errors='ignore'))
+		#print data
+		conditions_count =  len(data["data"]["conditions"])
+		j = 0
+		for j in range(j,conditions_count):
+			data2 = data["data"]["conditions"][j]["name"]
+			print j,". ",data2
+			sheet.write(j+1,i,data2)
+			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_55_64.xls')
+
+workbook = xlwt.Workbook()
+
+for k in range(1,69):
+	url_symptoms = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/symptoms"
+	data_symptoms = {"request":{"user":{"age":65,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","bodypartid":k}}
+	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	r = requests.post(url_symptoms, data=json.dumps(data_symptoms), headers=headers)
+	r.encoding = None
+	data = json.loads(r.text.encode(encoding='ISO-8859-1',errors='ignore'))
+
+	c = len(data["data"]["symptoms"])
+	i = 0
+	l_s_id = []
+	l_s_nm = []
+	for i in range(i,c):
+		data1 = data["data"]["symptoms"][i]["id"]
+		name1 = data["data"]["symptoms"][i]["nm"]
+		l_s_id.append(data1)
+		l_s_nm.append(name1)
+		#time.sleep(2)
+	#print l_s_id
+	#print l_s_nm
+
+	sheet = workbook.add_sheet("BodyPart_"+str(k))
+	style = xlwt.easyxf('font: bold 1')
+
+	print "Body Part : ",k,"\n\n"
+
+	i = 0
+
+	for i in range(i,c):
+		data_id = l_s_id[i]
+		symptom_name = l_s_nm[i]
+		sheet.write(0,i,symptom_name,style)
+		workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_65_plus.xls')
+		print "Symptom ",i," : ", symptom_name
+		url_conditions = "http://symptoms.webmd.com/scapp/SymptomCheckerAPI.svc/conditions"
+		data_conditions = {"request":{"user":{"age":65,"gender":"F","zip":"","vid":"cfd87d3c-3f57-4a36-acdf-11553e89255f"},"locale":"us","maxconditions":200,"bodyparts":[{"id":k,"symptoms":[{"id":data_id,"qclss":[{"quals":[]}]}]}]}}
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		r = requests.post(url_conditions, data=json.dumps(data_conditions), headers=headers)
+		r.encoding = None
+		data = json.loads(r.text.encode(encoding='ISO-8859-1',errors='ignore'))
+		#print data
+		conditions_count =  len(data["data"]["conditions"])
+		j = 0
+		for j in range(j,conditions_count):
+			data2 = data["data"]["conditions"][j]["name"]
+			print j,". ",data2
+			sheet.write(j+1,i,data2)
+			workbook.save('/home/ubuntu/webmed/webmed-symptoms/female_65_plus.xls')
